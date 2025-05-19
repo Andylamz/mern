b1878b96db547dcb99787797bba129d3edb25723
Andy <andy.lamyt@gmail.com>
2025-05-20 00:56:52 +0100

    added all basic API for /api/products

--- server.js
+++ server.js
@@ -5,20 +5,90 @@ import ProductModel from "./models/product.js";
 
 dotenv.config();
 const app = express();
+// parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.post("/products", async (req, res) => {
+app.post("/api/products", async (req, res) => {
   const product = req.body;
+  console.log(product);
 
   if (!product.name || !product.price || !product.image) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Please provide all fields" });
+    return res.status(400).json({
+      success: false,
+      message: "Please provide all fields",
+      data: null,
+    });
   }
 
   try {
     const newProduct = await ProductModel.create({ ...product });
     console.log(newProduct);
-  } catch (err) {}
+    return res.status(201).json({ success: true, data: newProduct });
+  } catch (err) {
+    console.error("Error in Create product:", err.message);
+    return res
+      .status(500)
+      .json({ success: false, message: "Server Error", data: null });
+  }
+});
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const allProducts = await ProductModel.find();
+    console.log(allProducts);
+    return res.status(201).json({ success: true, data: allProducts });
+  } catch (err) {
+    console.error("Error in Create product:", err.message);
+    return res
+      .status(500)
+      .json({ success: false, message: "Server Error", data: null });
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const deleteItem = await ProductModel.findByIdAndDelete(id);
+    console.log(deleteItem);
+    if (!deleteItem) {
+      return res.status(400).json({
+        success: false,
+        message: "Cannot find item",
+        data: null,
+      });
+    }
+    return res
+      .status(200)
+      .json({ success: true, message: "deleted", data: deleteItem });
+  } catch (err) {
+    console.error("Error in Deleting product:", err.message);
+    return res
+      .status(500)
+      .json({ success: false, message: "Server Error", data: null });
+  }
+});
+
+app.patch("/api/products/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const data = await ProductModel.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (!data) {
+      return res.status(400).json({
+        success: false,
+        message: "Cannot find item",
+        data: null,
+      });
+    }
+    return res.status(200).json({ success: true, message: "updated", data });
+  } catch (err) {
+    console.error("Error in Deleting product:", err.message);
+    return res
+      .status(500)
+      .json({ success: false, message: "Server Error", data: null });
+  }
 });
 
 app.listen(5000, () => {
