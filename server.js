95e022a8ce04fe35cf696ac46394fd95e31bd82a
Andy <andy.lamyt@gmail.com>
2025-05-20 12:40:15 +0100

    added environment variable PORT

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ import dotenv from "dotenv";
 
 import { connectDB } from "./config/db.js";
 import productPage from "./routes/productRoutes.js";
-import ProductModel from "./models/product.js";
 
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT;
 // parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/api/products", productPage);
 
-app.listen(5000, () => {
+app.listen(PORT || 5000, () => {
   connectDB();
   console.log("Server started at http://localhost:5000");
 });
