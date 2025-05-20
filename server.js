7204d7e8fb33289a62974ee1ba3ebe6964c01fd6
Andy <andy.lamyt@gmail.com>
2025-05-20 23:11:04 +0100

    added cors middleware (just for localhost now, no config)

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import cors from "cors";
 import { connectDB } from "./config/db.js";
 import productPage from "./routes/productRoutes.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
+
+// CORS
+app.use(cors());
 // parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
