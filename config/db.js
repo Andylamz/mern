bef5f2bbae4a2749054d19deabf337c545c96661
Andy <andy.lamyt@gmail.com>
2025-05-21 15:56:40 +0100

    added .gitkeep

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 export const connectDB = async () => {
   try {
     const connect = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected: ${connect.connection.host}`);
-  } catch (err) {}
+  } catch (err) {
+    console.err(`Error: ${err.message}`);
+    process.exit(1);
+  }
 };
