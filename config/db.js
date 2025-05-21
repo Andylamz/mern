c1c30ec6dd8e732d7722db933d5e4f10188302be
Andy <andy.lamyt@gmail.com>
2025-05-21 16:14:42 +0100

    remove sensitive info

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     const connect = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB connected: ${connect.connection.host}`);
+    console.log(`MongoDB connected`);
   } catch (err) {
     console.err(`Error: ${err.message}`);
     process.exit(1);
